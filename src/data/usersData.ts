2fa03f2b85e4fff642b1c8b7c912a587057847e6
camomile722 <101259281+AllaBru@users.noreply.github.com>
2023-09-18 19:04:24 +0200

    fix: deployment

---
+++ src/data/usersData.ts
@@ -0,0 +1,38 @@
+export const usersData = [
+    {
+        id: "1",
+        image: {
+            name: "Kent Dodds",
+            url: "https://bit.ly/kent-c-dodds",
+        },
+    },
+
+    {
+        id: "2",
+        image: {
+            name: "Kola Tioluwani",
+            url: "https://bit.ly/tioluwani-kolawole",
+        },
+    },
+    {
+        id: "3",
+        image: {
+            name: "Dan Abrahmov",
+            url: "https://bit.ly/dan-abramov",
+        },
+    },
+    {
+        id: "4",
+        image: {
+            name: "Ryan Florence",
+            url: "https://bit.ly/ryan-florence",
+        },
+    },
+    {
+        id: "5",
+        image: {
+            name: "Prosper Otemuyiwa",
+            url: "https://bit.ly/prosper-baba",
+        },
+    },
+];
